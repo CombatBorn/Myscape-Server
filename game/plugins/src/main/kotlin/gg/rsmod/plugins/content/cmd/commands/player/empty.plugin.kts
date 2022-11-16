3b441bf989ceba565e8521cb8e73dd2c38c3ca4b
CombatBorn <HunterGaynon@gmail.com>
2022-11-16 04:54:46 +0100

    added warning message

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/commands/player/empty.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/commands/player/empty.plugin.kts
@@ -1,3 +1,10 @@
-on_command("empty") {
-    player.inventory.removeAll()
+on_command("empty", description = "Empty all items from inventory") {
+    player.queue(TaskPriority.STRONG) {
+        emptyInventoryRequest()
+    }
+}
+suspend fun QueueTask.emptyInventoryRequest() {
+    when (options("yes", "no", title = "Empty all items in your inventory?")) {
+        1 -> player.inventory.removeAll()
+    }
 }
