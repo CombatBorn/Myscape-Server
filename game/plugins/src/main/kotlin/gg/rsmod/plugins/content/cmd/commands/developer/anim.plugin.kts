069aecc3cb815efc06aa64339c72bedbc784bfd7
CombatBorn <HunterGaynon@gmail.com>
2022-11-16 04:57:07 +0100

    added ::findanim command

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/commands/developer/anim.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/commands/developer/anim.plugin.kts
@@ -11,3 +11,27 @@ on_command("anim", Privilege.DEV_POWER, description = "Play animation") {
         player.message("Animate: $id")
     }
 }
+on_command("findanim", Privilege.DEV_POWER, description = "Find animations for first item in inventory") {
+    var animFound = -1
+    val id = if (player.getCommandArgs().isEmpty()) {
+        player.inventory.first()?.id ?: 2309
+    } else {
+        player.getCommandArgs()[0].toInt()
+    }
+    for (index in 0..world.definitions.getCount(AnimDef::class.java)) {
+        if (!world.definitions.contains(AnimDef::class.java, index)) {
+            continue
+        }
+        val animation = world.definitions.get(AnimDef::class.java, index)
+        if (animation.leftHandItem == id || animation.rightHandItem == id) {
+            player.message("Animation ${animation.id} uses ${world.definitions.get(ItemDef::class.java, id).name} ($id) in the animation")
+            if (animFound == -1) animFound = animation.id
+        }
+    }
+    if (animFound == -1) {
+        player.message("No animations were found for ${world.definitions.get(ItemDef::class.java, id).name}")
+    } else {
+        player.message("Playing animation \"$animFound\"")
+        player.animate(animFound)
+    }
+}
