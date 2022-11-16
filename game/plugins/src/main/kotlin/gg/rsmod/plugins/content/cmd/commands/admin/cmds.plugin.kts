372d0a430ba9bed8c6437122de218f919f6d3e3a
CombatBorn <HunterGaynon@gmail.com>
2022-11-16 04:55:42 +0100

    added command description, changed from ::cmds to ::commands

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/commands/admin/cmds.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/commands/admin/cmds.plugin.kts
@@ -1,6 +1,6 @@
 import gg.rsmod.game.model.priv.Privilege
 
-on_command(command = "cmds", powerRequired = Privilege.ADMIN_POWER, description = "Display commands") {
+on_command(command = "commands", powerRequired = Privilege.ADMIN_POWER, description = "Display all commands") {
     player.setInterfaceUnderlay(color = -1, transparency = -1)
     player.openInterface(187, dest = InterfaceDestination.MAIN_SCREEN)
 
