829d0b13123e5e3b75f3177b9caf00b8ba2acc2f
CombatBorn <minecraftisreality@gmail.com>
2022-12-10 18:05:34 +0100

    added formatting, debugging, and interface events

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/area/entrana/vannaka.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/area/entrana/vannaka.plugin.kts
@@ -13,22 +13,22 @@ on_npc_option(Npcs.VANNAKA, "Talk-to") {
         when (options("Get assignment", "View shop", "View perks", "Favorite/block tasks")) {
             1 -> {
                 chatPlayer("I'd like a slayer assignment", animation = 588)
-                player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
+                openSlayerInterface(player,1)
             }
 
             2 -> {
                 chatPlayer("What do you have for sale?", animation = 567)
-                player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
+                openSlayerInterface(player,3)
             }
 
             3 -> {
                 chatPlayer("What perks do you provide?", animation = 567)
-                player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
+                openSlayerInterface(player,2)
             }
 
             4 -> {
-                chatPlayer("What perks do you provide?", animation = 567)
-                player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
+                chatPlayer("Can I favorite/Block tasks?", animation = 567)
+                openSlayerInterface(player,4)
             }
         }
 
@@ -38,11 +38,26 @@ on_npc_option(Npcs.VANNAKA, "Assignment") {
     player.queue {
         chatNpc("You may choose between Easy, Medium, Hard, or Boss tasks.", Npcs.VANNAKA)
         player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
+        openSlayerInterface(player,1)
     }
 }
 on_npc_option(Npcs.VANNAKA, "Trade") {
     player.queue {
         chatNpc("Sure, take a look at my goods.", Npcs.VANNAKA)
-        player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
+        openSlayerInterface(player,3)
+    }
+}
+
+fun openSlayerInterface(player: Player, tab: Int){
+    player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
+    // TODO: runClientScript to select which tab is opened
+    if (tab == 2){
+        player.setInterfaceEvents(interfaceId = 5000, component = 193, range = 0..24, InterfaceEvent.BUTTON1, InterfaceEvent.BUTTON10)
+    }
+}
+on_button(5000, 193) {
+    when (player.getInteractingOption()) {
+        1 -> player.message("You clicked component 193 option: ${player.getInteractingOption()} slot: ${player.getInteractingSlot()}")
+        10 -> player.message("You clicked component 193 option: ${player.getInteractingOption()} slot: ${player.getInteractingSlot()}")
     }
 }
