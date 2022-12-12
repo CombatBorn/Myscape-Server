b585223391af9b04b600f8fa992fc7efe7d4461e
CombatBorn <HunterGaynon@gmail.com>
2022-12-12 11:28:04 +0100

    removed debug, added totalItems variable

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/area/entrana/vannaka.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/area/entrana/vannaka.plugin.kts
@@ -2,12 +2,6 @@ spawn_npc(Npcs.VANNAKA, 2848, 3341, 0, 0)
 
 on_npc_option(Npcs.VANNAKA, "Talk-to") {
 
-    if (world.definitions.get(NpcDef::class.java, Npcs.VANNAKA).models != null) {
-        for (model in world.definitions.get(NpcDef::class.java, Npcs.VANNAKA).models!!) {
-            player.message("Model ID: $model")
-        }
-    }
-
     player.queue {
         chatNpc("What can I help you with?", Npcs.VANNAKA)
         when (options("Get assignment", "View shop", "View perks", "Favorite/block tasks")) {
@@ -15,17 +9,14 @@ on_npc_option(Npcs.VANNAKA, "Talk-to") {
                 chatPlayer("I'd like a slayer assignment", animation = 588)
                 openSlayerInterface(player,1)
             }
-
             2 -> {
                 chatPlayer("What do you have for sale?", animation = 567)
                 openSlayerInterface(player,3)
             }
-
             3 -> {
                 chatPlayer("What perks do you provide?", animation = 567)
                 openSlayerInterface(player,2)
             }
-
             4 -> {
                 chatPlayer("Can I favorite/Block tasks?", animation = 567)
                 openSlayerInterface(player,4)
@@ -48,11 +39,12 @@ on_npc_option(Npcs.VANNAKA, "Trade") {
     }
 }
 
-fun openSlayerInterface(player: Player, tab: Int){
+fun openSlayerInterface(player: Player, tab: Int) {
     player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
     // TODO: runClientScript to select which tab is opened
-    if (tab == 2){
-        player.setInterfaceEvents(interfaceId = 5000, component = 193, range = 0..74, InterfaceEvent.BUTTON1, InterfaceEvent.BUTTON10)
+    if (tab == 2) {
+        val totalItems = 50;
+        player.setInterfaceEvents(interfaceId = 5000, component = 195, range = 0..(totalItems * 3 - 3), InterfaceEvent.BUTTON1, InterfaceEvent.BUTTON10)
     }
 }
 on_button(5000, 193) {
