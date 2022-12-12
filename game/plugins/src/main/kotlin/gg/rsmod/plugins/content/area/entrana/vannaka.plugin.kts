d36971725304208add3f0d1936923e24efa37357
CombatBorn <minecraftisreality@gmail.com>
2022-12-12 21:06:47 +0100

    added slayer shop

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/area/entrana/vannaka.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/area/entrana/vannaka.plugin.kts
@@ -7,19 +7,19 @@ on_npc_option(Npcs.VANNAKA, "Talk-to") {
         when (options("Get assignment", "View shop", "View perks", "Favorite/block tasks")) {
             1 -> {
                 chatPlayer("I'd like a slayer assignment", animation = 588)
-                openSlayerInterface(player,1)
+                openSlayerInterface(player, 1)
             }
             2 -> {
                 chatPlayer("What do you have for sale?", animation = 567)
-                openSlayerInterface(player,3)
+                openSlayerInterface(player, 3)
             }
             3 -> {
                 chatPlayer("What perks do you provide?", animation = 567)
-                openSlayerInterface(player,2)
+                openSlayerInterface(player, 2)
             }
             4 -> {
                 chatPlayer("Can I favorite/Block tasks?", animation = 567)
-                openSlayerInterface(player,4)
+                openSlayerInterface(player, 4)
             }
         }
 
@@ -29,24 +29,57 @@ on_npc_option(Npcs.VANNAKA, "Assignment") {
     player.queue {
         chatNpc("You may choose between Easy, Medium, Hard, or Boss tasks.", Npcs.VANNAKA)
         player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
-        openSlayerInterface(player,1)
+        openSlayerInterface(player, 1)
     }
 }
 on_npc_option(Npcs.VANNAKA, "Trade") {
     player.queue {
         chatNpc("Sure, take a look at my goods.", Npcs.VANNAKA)
-        openSlayerInterface(player,3)
+        openSlayerInterface(player, 3)
+    }
+}
+on_npc_option(Npcs.VANNAKA, "Rewards") {
+    player.queue {
+        chatNpc("Sure, take a look at my goods.", Npcs.VANNAKA)
+        openSlayerInterface(player, 2)
     }
 }
 
 fun openSlayerInterface(player: Player, tab: Int) {
     player.openInterface(interfaceId = 5000, InterfaceDestination.MAIN_SCREEN)
-    // TODO: runClientScript to select which tab is opened
-    if (tab == 2) {
-        val totalItems = 50;
-        player.setInterfaceEvents(interfaceId = 5000, component = 195, range = 0..(totalItems * 3 - 3), InterfaceEvent.BUTTON1, InterfaceEvent.BUTTON10)
+    player.runClientScript(30010, tab)
+    // Slayer Rewards Shop (Slayer points)
+    var totalItems = 50;
+    player.setInterfaceEvents(interfaceId = 5000, component = 98, range = 0..(totalItems * 3 - 3), InterfaceEvent.BUTTON1, InterfaceEvent.BUTTON10)
+    // Slayer items Shop (Coins)
+    totalItems = 11;
+    player.setInterfaceEvents(interfaceId = 5000, component = 104, range = 0..(totalItems * 3 - 3), InterfaceEvent.BUTTON1, InterfaceEvent.BUTTON10)
+}
+
+val slayerItems = listOf(4155, 4156, 4162, 4164, 4166, 4168, 4170, 4551, 6708, 7159, 23037)
+val slayerItemsPrice = listOf(1, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000)
+on_button(5000, 104) {
+    val slot = player.getInteractingSlot() / 3
+    val itemId = slayerItems[slot]
+    val price = slayerItemsPrice[slot]
+    if (player.getInteractingOption() == 1) {
+        val removed = player.inventory.remove(995, price).completed
+        if (removed == price){
+            val given = player.inventory.add(itemId).completed
+            if (given == 1){
+                player.message("You purchased ${world.definitions.get(ItemDef::class.java, itemId).name}.")
+                return@on_button
+            } else {
+                player.message("You do not have enough inventory space for this.")
+                player.inventory.add(995, removed)
+            }
+        } else{
+            player.inventory.add(995, removed)
+            player.message("You do not have enough coins for this.")
+        }
+    }else if (player.getInteractingOption() == 10){
+        player.message("${world.definitions.get(ItemDef::class.java, itemId).examine}")
     }
 }
-on_button(5000, 193) {
-    player.message("You clicked component 193 option: ${player.getInteractingOption()} slot: ${player.getInteractingSlot()}")
-}
+
+
