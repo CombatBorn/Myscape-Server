77b063c659c4e5c5d5b4dbb54998ed44ddccc470
CombatBorn <minecraftisreality@gmail.com>
2023-01-28 16:51:08 +0100

    added test npcs

--- game/src/main/kotlin/gg/rsmod/game/service/cachemodifier/configurations/CustomNpcs.kt
+++ game/src/main/kotlin/gg/rsmod/game/service/cachemodifier/configurations/CustomNpcs.kt
@@ -6,7 +6,7 @@ enum class CustomNpcs (
     val npcName: String,
     val models: Array<Int>? = null,
     val category: Int = -1,
-    val size: Int = -1,
+    val size: Int = 1,
     val standAnim: Int = -1,
     val walkAnim: Int = -1,
     val rotateLeftAnim: Int = -1,
@@ -51,13 +51,18 @@ enum class CustomNpcs (
         copyNpc = 2085,
         id = 11464,
         npcName = "Air Giant",
-        recolors = arrayListOf(Pair(21619, 33719), Pair(32461, 34743), Pair(32448, 36791), Pair(32308, 37815), Pair(32424, 39863), Pair(14144, 33719), Pair(156, 40887), Pair(16067, 33719), Pair(32585, 35767), Pair(32537, 41911), Pair(32308, 37815), Pair(32448, 36791), Pair(32461, 34743), Pair(32424, 39863), Pair(898, 43959))
+        recolors = arrayListOf(Pair(21619, 114), Pair(33075, 55), Pair(33088, 69), Pair(33228, 81), Pair(33112, 91), Pair(51392, 66), Pair(156, 27), Pair(49469, 61), Pair(32951, 58), Pair(32999, 104), Pair(33228, 81), Pair(33088, 69), Pair(33075, 55), Pair(33112, 91), Pair(898, 1))
     ),
     ICE_DRAGON(
         copyNpc = 260,
         id = 11465,
         npcName = "Ice Dragon",
-        recolors = arrayListOf(Pair(1938, 41911), Pair(910, 42935), Pair(912, 42935), Pair(1814, 40887), Pair(0, 43957), Pair(809, 39863), Pair(5198, 34743), Pair(5206, 33719), Pair(2469, 38839), Pair(2588, 39863), Pair(5219, 33719), Pair(5214, 33719), Pair(27417, 41911))
+        recolors = arrayListOf(Pair(2469, 38839), Pair(912, 42935), Pair(1938, 41911), Pair(1814, 40887), Pair(2588, 39863), Pair(910, 42935))
+    ),
+    ROCK_GIANT(
+        copyNpc = 2075,
+        id = 11466,
+        npcName = "Rock Giant",
+        recolors = arrayListOf(Pair(1560, 2328), Pair(2853, 2215), Pair(2716, 1192), Pair(2977, 154), Pair(7993, 99), Pair(1556, 19), Pair(262, 1291), Pair(1808, 47), Pair(654, 37), Pair(652, 1189), Pair(4, 1056), Pair(522, 19), Pair(902, 9), Pair(8010, 113), Pair(520, 148), Pair(2714, 61), Pair(10314, 10314), Pair(2857, 45), Pair(70, 70))
     )
-
 }
