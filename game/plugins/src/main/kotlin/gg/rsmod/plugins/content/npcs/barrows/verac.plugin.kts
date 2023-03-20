9f265efbd00e4853ad10c159fe710f23db6b3ac6
CombatBorn <minecraftisreality@gmail.com>
2023-03-20 14:30:23 +0100

    removed barrows npc spawns

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/barrows/verac.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/barrows/verac.plugin.kts
@@ -1,11 +1,5 @@
 package gg.rsmod.plugins.content.npcs.barrows
 
-spawn_npc(Npcs.VERAC_THE_DEFILED, 3557, 3297, 0, 2);
-spawn_npc(Npcs.VERAC_THE_DEFILED, 3559, 3300, 0, 2);
-spawn_npc(Npcs.VERAC_THE_DEFILED, 3555, 3297, 0, 2);
-spawn_npc(Npcs.VERAC_THE_DEFILED, 3555, 3294, 0, 2);
-spawn_npc(Npcs.VERAC_THE_DEFILED, 3559, 3294, 0, 2);
-
 set_combat_def(Npcs.VERAC_THE_DEFILED) {
     configs {
         attackSpeed = 6
